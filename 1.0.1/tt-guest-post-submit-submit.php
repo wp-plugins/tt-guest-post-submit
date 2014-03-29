35b3bca3e42ed008b42723ce35d44dca81897677
rashed.latif <rashed.latif@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-29 11:40:05 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/tt-guest-post-submit/tags@884104 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 1.0.1/tt-guest-post-submit-submit.php
+++ 1.0.1/tt-guest-post-submit-submit.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require_once($_POST["rootpath"]);
+    require_once($_SESSION['rootpath']);
     $title = $_POST["title"];
     $content = $_POST["content"];
     $tags = $_POST["tags"];
@@ -10,58 +10,50 @@
     $authorid = $_POST["authorid"];
     $category = ($_POST['catdrp']==-1) ? array(1) : array($_POST['catdrp']);
     $redirect_location = $_POST["redirect_url"];
-    $path = $_POST["rootpath"];
     $nonce=$_POST["_wpnonce"];
     
     if(isset($_POST['submit'])) {
-        if(!empty($_POST['author']) && !empty($_POST['email']) && !empty($_POST['content']) && !empty($_POST['code'])) {
-            if($_POST['code'] == $_SESSION['random_code']) {
-                if (! wp_verify_nonce($nonce) ) die('Security check'); 
-                $new_post = array(
-                        'post_title'    => $title,
-                        'post_content'  => $content,
-                        'post_category' => $category,  // Usable for custom taxonomies too
-                        'tags_input'    => $tags,
-                        'post_status'   => 'pending',           // Choose: publish, preview, future, draft, etc.
-                        'post_type' => 'post',  //'post',page' or use a custom post type if you want to
-                        'post_author' => $authorid //Author ID
-                );
-                $pid = wp_insert_post($new_post);
-                add_post_meta($pid, 'author', $author, true);
-                add_post_meta($pid, 'author-email', $email, true);
-                add_post_meta($pid, 'author-website', $site, true);
+        
+        if (! wp_verify_nonce($nonce) ) die('Security check'); 
+        $new_post = array(
+            'post_title'    => $title,
+            'post_content'  => $content,
+            'post_category' => $category,  // Usable for custom taxonomies too
+            'tags_input'    => $tags,
+            'post_status'   => 'pending',           // Choose: publish, preview, future, draft, etc.
+            'post_type' => 'post',  //'post',page' or use a custom post type if you want to
+            'post_author' => $authorid //Author ID
+        );
+        
+        $pid = wp_insert_post($new_post);
+        add_post_meta($pid, 'author', $author, true);
+        add_post_meta($pid, 'author-email', $email, true);
+        add_post_meta($pid, 'author-website', $site, true);
                 
-                if ( ! function_exists( 'wp_handle_upload' ) ) require_once( ABSPATH . 'wp-admin/includes/file.php' );
+        if ( ! function_exists( 'wp_handle_upload' ) ) require_once( ABSPATH . 'wp-admin/includes/file.php' );
                 
-                $uploadedfile = $_FILES['featured-img'];
-                $upload_overrides = array( 'test_form' => false );
-                $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
+        $uploadedfile = $_FILES['featured-img'];
+        $upload_overrides = array( 'test_form' => false );
+        $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
                 
-                if ( $movefile ) {
-                    $wp_filetype = $movefile['type'];
-                    $filename = $movefile['file'];
-                    $wp_upload_dir = wp_upload_dir();
-                    $attachment = array(
+        if ($movefile){
+            $wp_filetype = $movefile['type'];
+            $filename = $movefile['file'];
+            $wp_upload_dir = wp_upload_dir();
+            $attachment = array(
                         'guid' => $wp_upload_dir['url'] . '/' . basename( $filename ),
                         'post_mime_type' => $wp_filetype,
                         'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
                         'post_content' => '',
                         'post_status' => 'inherit'
                     );
-                    $attach_id = wp_insert_attachment( $attachment, $filename, $pid);
+            $attach_id = wp_insert_attachment( $attachment, $filename, $pid);
                     
-                    require_once( ABSPATH . 'wp-admin/includes/image.php' );
-                    $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
-                    wp_update_attachment_metadata( $attach_id, $attach_data );
-                    set_post_thumbnail($pid, $attach_id);     
-                }
-                header("Another Page");
-                header("Location: $redirect_location");
-            } else {
-                $error = "Please verify that you typed in the correct code.";
-            }
-        } else {
-            $error = "Please fill out the entire form.";
+            require_once( ABSPATH . 'wp-admin/includes/image.php' );
+            $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+            wp_update_attachment_metadata( $attach_id, $attach_data );
+            set_post_thumbnail($pid, $attach_id);     
         }
+        header("Location: $redirect_location");
     }  
 ?>
